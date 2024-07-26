d2996217c0fd7b96d94c7ca7094c619c5e532625
Simon Scabello <simon.scabello@live.com>
2024-07-26 11:25:42 -0300

    Adiciona helmet.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const todoRoutes = require('./routes/todoRoutes');
 const healthRoutes = require('./routes/healthRoutes');
 const authRoutes = require('./routes/authRoutes');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(helmet());
 app.use('/api/todo', todoRoutes);
 app.use('/api/health', healthRoutes);
 app.use('/api/auth', authRoutes);
