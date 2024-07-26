1582f799b41676c01d3778a252eae4d141c2f85e
Simon Scabello <simon.scabello@live.com>
2024-07-26 07:40:10 -0300

    Adiciona variaveis de ambiente ao projeto.

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('todo', 'root', 'root', {
-    // host: '172.17.0.2',
-    host: 'host.docker.internal',
-    dialect: 'mysql'
+const db_name = process.env.DB_NAME;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+const db_host = process.env.DB_HOST;
+const db_dialect = process.env.DB_DRIVER;
+
+const sequelize = new Sequelize(db_name, db_user, db_password, {
+    host: db_host,
+    dialect: db_dialect
 });
 
 module.exports = sequelize;
